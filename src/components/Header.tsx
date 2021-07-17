32f3a2038ed2a1590e4af218fec995bcb94d03b2
test <test@gmail.com>
2021-07-17 10:12:16 +0300

    first commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = () => {
+    return <header className='header'>
+        <img src = 'https://m.media-amazon.com/images/I/41i4xgZG36L._AC_SY355_.jpg'/>
+    </header>
+}
+
+
+export default Header();
