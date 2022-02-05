cf2efeb62247ce229012050806f57d78384501a2
ivanin_j <ivanin.jay@gmail.com>
2022-02-05 19:21:39 +0200

    small tests

--- src/components/Profile/ProfileInfo/ProfileStatus.test.tsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.test.tsx
@@ -3,7 +3,7 @@ import ProfileStatus from "./ProfileStatus";
 import {create} from "react-test-renderer";
 
 
-let updateStatus = ()=>{
+let updateStatus = () => {
 
 }
 
@@ -14,4 +14,39 @@ describe('Profile component', () => {
         const root = component.root;
         expect(root.instance.state.status).toBe('it-kamasutra.com');
     })
+
+    test('after creation input', () => {
+        // @ts-ignore
+        const component = create(<ProfileStatus status='it-kamasutra.com' updateStatus={updateStatus}/>);
+        const root = component.root;
+        expect(()=>{
+            let input = root.findByType('input')
+        }).toThrow();
+    })
+    test('auto creation span should contains correct status', () => {
+        // @ts-ignore
+        const component = create(<ProfileStatus status='it-kamasutra.com' updateStatus={updateStatus}/>);
+        const root = component.root;
+        let span = root.findByType('span')
+        expect(span.children[0]).toBe('it-kamasutra.com');
+    })
+    test('input should be displayed in editMode instead of span', () => {
+        // @ts-ignore
+        const component = create(<ProfileStatus status='it-kamasutra.com' updateStatus={updateStatus}/>);
+        const root = component.root;
+        let span = root.findByType('span')
+        span.props.onDoubleClick();
+        let input = root.findByType('input')
+        expect(input.props.value).toBe('it-kamasutra.com');
+    })
+    test('callback should be called', () => {
+        // @ts-ignore
+        let mockCallBack = jest.fn()
+        const component = create(<ProfileStatus status='it-kamasutra.com' updateStatus={mockCallBack}/>);
+        const instance = component.getInstance();
+        // @ts-ignore
+        instance.deActivateEditMode();
+        expect(mockCallBack.mock.calls.length).toBe(1);
+    })
+
 })
