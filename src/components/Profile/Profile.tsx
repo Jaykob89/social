c9d2e51e196b3df530e672ed1f9f01664266539b
ivanin_j <ivanin.jay@gmail.com>
2022-08-01 22:49:47 +0300

    Lesson 97

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import ProfileInfo from "./ProfileInfo/ProfileInfo";
 import MyPostsContainer from "./MyPosts/MyPostsContainer";
+import ProfileInfo from "./ProfileInfo/ProfileInfo";
 
 export type photosType = {
     small: string
--- src/components/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -11,19 +11,19 @@ type propsType = {
     status: string
     updateStatus: (status: string) => void
     isOwner: boolean
-    savePhoto:(file:File)=>void
+    savePhoto: (file: File) => void
 }
 
-const ProfileInfo = ({profile, status, updateStatus, isOwner,savePhoto}: propsType) => {
+const ProfileInfo = ({profile, status, updateStatus, isOwner, savePhoto}: propsType) => {
 
     if (!profile) {
         return <Preloader/>
     }
 
-    const onMainPhotoSelected =(e:ChangeEvent<HTMLInputElement>)=>{
-         if (e.target.files) {
-             savePhoto(e.target.files[0])
-         }
+    const onMainPhotoSelected = (e: ChangeEvent<HTMLInputElement>) => {
+        if (e.target.files) {
+            savePhoto(e.target.files[0])
+        }
     }
 
     return <div>
@@ -31,12 +31,38 @@ const ProfileInfo = ({profile, status, updateStatus, isOwner,savePhoto}: propsTy
         <div className={s.descriptionBlock}>
             <img alt={'avatar_photo'} src={profile.photos.large || userPhoto}/>
             {isOwner && <input type={'file'} onChange={onMainPhotoSelected}/>}
-            <div>{profile.fullName}</div>
-            <div>{profile.aboutMe}</div>
-            <ProfileStatusWithHooks status={status} updateStatus={updateStatus} />
+            <ProfileData profile={profile}/>
+            <ProfileStatusWithHooks status={status} updateStatus={updateStatus}/>
         </div>
     </div>
 }
 
 
-export default ProfileInfo;
+const ProfileData = ({profile}: any) => {
+    return <div>
+        <div>{profile.fullName}</div>
+        <div>{profile.aboutMe}</div>
+        <div>Looking for a job : {profile.lookingForAJob ? "yes" : 'no'}</div>
+        {
+            profile.lookingForAJobDescription &&
+            <div>
+                My professional skills : {profile.lookingForAJobDescription}
+            </div>
+        }
+        <div>About me : {profile.aboutMe}</div>
+        <div>Contacts :{Object.keys(profile.contacts).map(key => {
+            return <Contact contactTitle={key} contactValue={profile.contacts[key]}/>
+        })}</div>
+    </div>
+}
+
+type ContactType = {
+    [key: string]: string
+}
+
+const Contact = ({contactTitle, contactValue}: ContactType) => {
+    return <div>{contactTitle}:{contactValue}</div>
+}
+
+
+export default ProfileInfo
